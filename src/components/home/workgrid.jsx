b7fbda036f00e32f4c1dad5e1b74d21a8453cc62
Ashish <kapoorashish713@gmail.com>
2026-02-15 21:47:50 +0530

    font

--- src/components/home/workgrid.jsx
+++ src/components/home/workgrid.jsx
@@ -6,11 +6,11 @@ export default function WorkGrid() {
     <section className="py-24  border-t border-black/5 dark:border-white/5 w-full">
       <div className="max-w-screen-2xl mx-auto">
        <div className="w-full p-1 flex sm:flex-row border-b border-neutral-700 flex-col sm:justify-between items-center">
-         <h2 className="font-display text-4xl min-[264px]:text-6xl md:text-8xl uppercase text-center pb-4">
+         <h2 style={{ fontFamily: "Poppins" }} className="font-display text-4xl min-[264px]:text-6xl md:text-8xl uppercase text-center pb-4">
           CURATED <br />
-          (WORK)
+          WORK
         </h2>
-        <span className="font-display text-4xl  min-[264px]:text-6xl  md:text-8xl ">12-25</span>
+        <span style={{ fontFamily: "Poppins" }} className="font-display text-4xl  min-[264px]:text-6xl  md:text-8xl ">12-25</span>
        </div>
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-9 gap-2 p-2">
           {works.map((work) => (
